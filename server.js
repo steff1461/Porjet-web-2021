0198a857fe748ef1a69d37a1acb6734f8ee6ec19
steff1461 <steffen.w@outlook.be>
2021-04-18 21:11:12 +0200

    dfghb

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors = require("cors");
 
 const app = express();
+
+// add this code
+const whitelist = ['*']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+// end
+app.use(cors(corsOptions));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/williamFront'));
