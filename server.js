0b84a94c38763061aabad3bed521fc246eb933c7
steff1461 <steffen.w@outlook.be>
2021-04-19 09:33:20 +0200

    srgfrd

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 // // add this code
-// const whitelist = ['*']; // list of allow domain
-//
-// const corsOptions = {
-//     origin: function (origin, callback) {
-//         if (!origin) {
-//             return callback(null, true);
-//         }
-//
-//         if (whitelist.indexOf(origin) === -1) {
-//             var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     }
-// }
-//
-// app.get(cors(corsOptions));
-// // end
-// app.use(cors(corsOptions));
+const whitelist = ['*']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+app.get(cors(corsOptions));
+// end
+app.use(cors(corsOptions));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/williamFront'));
